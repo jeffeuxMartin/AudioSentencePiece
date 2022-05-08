537f95ac389de079fca40d4554e275173c0341cb
Jeff Chen <r09942097@ntu.edu.tw>
2022-05-09 03:01:56 +0800

    _

--- scripts/torun_stack/STACK2.sh
+++ scripts/torun_stack/STACK2.sh
@@ -1,5 +1,5 @@
-python3 AudioSentencePiece/main.py -b 6 -B 4 --scratch --eval_steps 50 --metric_batch 4 --verbose_batch 0 --generation_max_length 512 --eval_accumulation_steps 10
-python3 AudioSentencePiece/main.py -b 6 -B 4 --scratch --eval_steps 50 --metric_batch 4 --verbose_batch 0 --generation_max_length 512 --eval_accumulation_steps 10 --dev_split dummy --nowandb
+# python3 AudioSentencePiece/main.py -b 6 -B 4 --scratch --eval_steps 50 --metric_batch 4 --verbose_batch 0 --generation_max_length 512 --eval_accumulation_steps 10
+# python3 AudioSentencePiece/main.py -b 6 -B 4 --scratch --eval_steps 50 --metric_batch 4 --verbose_batch 0 --generation_max_length 512 --eval_accumulation_steps 10 --dev_split dummy --nowandb
 
 
 #!sh
