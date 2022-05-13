1a57804bd5797419f92c59a6a76e5f6228091240
Jeff Chen <r09942097@ntu.edu.tw>
2022-05-14 00:02:56 +0800

    _

--- scripts/autobattleship.sh
+++ scripts/autobattleship.sh
@@ -65,7 +65,7 @@ VRAM_SIZE={vram_size}
   echo "你拿到 $GPU_COUNTS 張 GPU, 每張 VRAM = $VRAM_SIZE GB..."
 
   case Task in
-    AE ) datasize=6;;
+    AE ) datasize=12;;
     ASR) datasize=4;;
     ST ) datasize=2;;
     *  ) datasize=2;;
